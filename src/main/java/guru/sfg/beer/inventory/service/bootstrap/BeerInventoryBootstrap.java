75579814a425ea0089840d6f53b28e9ff53127c0
Andrija Musikic <andrija.musikic@quantox.com>
2023-10-09 19:30:19 +0200

    Remove test comment

--- src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
@@ -16,7 +16,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Component
 public class BeerInventoryBootstrap implements CommandLineRunner {
-    // test
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
